7571acb162abf589b7acfd77d854233814441a01
Booty King <b00tyking@protonmail.com>
2022-02-01 14:54:45 -0600

    On season.updateFighterRankings, set rank on each fighter

--- functions/src/season.ts
+++ functions/src/season.ts
@@ -24,6 +24,63 @@ export const updateCumulativeSeasonStats = async (seasonId: string, db: any) => 
     });
   } catch (error) {
     console.error(error);
-    throw new Error(`updateCumulativeSeasonStats failed`);
+    throw new Error(`updateCumulativeSeasonStats failed ${error}`);
   }
+};
+
+export const updateFighterRankings = async (seasonId: string, db: any) => {
+  const fightersPath = db
+    .collection('nft-death-games')
+    .doc(seasonId)
+    .collection('fighters');
+
+  try {
+    const allFighters = await fightersPath
+    .where('is_doping', '==', false)
+    .where('is_invalid', '==', false)
+    .where('statsDone', '==', true)
+    .get();
+
+    await Promise.all(allFighters
+      .map((fighter: any) => fighter.data())
+      .sort(compareFighters)
+      .map(async (fighter: any, index: number) => {
+        return await fightersPath
+          .doc(fighter.id)
+          .update({
+            ranking: index
+          });
+      }));
+  } catch (error) {
+    console.error(error);
+    throw new Error(`updateFighterRankings failed ${error}`);
+  }
+};
+
+export const compareFighters = (fighter1: any, fighter2: any) => {
+  const stats1 = fighter1.stats;
+  const stats2 = fighter2.stats;
+
+  const wins = stats1.won > stats2.won;
+  const kos = stats1.knockedOutOpponent > stats2.knockedOutOpponent;
+  const perfects = stats1.perfectedOpponent > stats2.perfectedOpponent;
+
+  // If the lower power fighter has the same # of wins, kos and perfects,
+  // they have heart. Rank them higher.
+  const power = fighter1.player.power < fighter2.player.power;
+
+  const dmgDealt = stats1.damageDealt > stats2.damageDealt;
+  const dmgReceived = stats1.damageReceived < stats2.damageReceived;
+
+  if (
+    wins ||
+    kos ||
+    perfects ||
+    power ||
+    dmgDealt ||
+    dmgReceived) {
+    return -1;
+  }
+
+  return 0;
 };
